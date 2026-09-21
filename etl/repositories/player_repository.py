6ea0bf8742d021144e7cd72a079e6d1044026d2c
rualpa2004 <146848892+rualpa2004@users.noreply.github.com>
2026-09-21 17:23:48 +0200

    Adding functions to get info from Mongo database

--- etl/repositories/player_repository.py
+++ etl/repositories/player_repository.py
@@ -9,3 +9,23 @@ def save_player(player: Player):
         {"$set": asdict(player)},
         upsert = True
     )
+
+def find_player_by_highlightly_id(highlightly_id):
+    db = get_database()
+    return db.players.find_one({"highlightly_id": highlightly_id})
+
+def find_player_by_name(name):
+    db = get_database()
+    return db.players.find_one({"name": name})
+
+def find_players_by_team(team_id):
+    db = get_database()
+    return list(db.players.find({"team_id": team_id}))
+
+def set_player_highlightly_id(external_id, highlightly_id):
+    db = get_database()
+    db.players.update_one({"external_id": external_id}, {"$set": {"highlightly_id": highlightly_id}})
+
+def find_all_players():
+    db = get_database()
+    return list(db.players.find({}))
--- etl/repositories/team_repository.py
+++ etl/repositories/team_repository.py
@@ -9,3 +9,19 @@ def save_team(team: Team):
         {"$set": asdict(team)},
         upsert = True
     )
+
+def find_team_by_highlightly_id(highlightly_id):
+    db = get_database()
+    return db.teams.find_one({"highlightly_id": highlightly_id})
+
+def find_team_by_name(name):
+    db = get_database()
+    return db.teams.find_one({"name": name})
+
+def set_team_highlightly_id(team_external_id, highlightly_id):
+    db = get_database()
+    return db.teams.update_one({"external_id": team_external_id}, {"$set": {"highlightly_id": highlightly_id}})
+
+def find_all_teams():
+    db = get_database()
+    return list(db.teams.find({}))
